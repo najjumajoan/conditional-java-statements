ba72f781c4d01ba985d3494de031254c423780ad
Najjuma joan <88086822+najjumajoan@users.noreply.github.com>
2021-09-26 19:13:57 +1200

    conditional statements

---
+++ src/com/example/damo/damo.java
@@ -0,0 +1,12 @@
+package com.example.damo;
+
+public class damo {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 18;
+        if (x > y) {
+            System.out.println("x is greater than y");
+        }
+
+    }
+}
